f73bf69a1000c0a94d05ffbe8ed8dc492e1e7847
Hugo Bray <hbray@f6r13s1.clusters.42paris.fr>
2026-02-24 15:45:18 +0100

    philo

--- check.c
+++ check.c
@@ -6,31 +6,22 @@
 /*   By: hbray <hbray@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 13:02:26 by hbray             #+#    #+#             */
-/*   Updated: 2026/02/24 15:11:35 by hbray            ###   ########.fr       */
+/*   Updated: 2026/02/24 15:44:40 by hbray            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	check_death(t_philo *philos)
+static int	process_death(t_philo *philos)
 {
-	long	last_meal;
-
-	pthread_mutex_lock(&philos->meal_lock);
-	last_meal = philos->last_meal_time;
-	pthread_mutex_unlock(&philos->meal_lock);
-	if (get_time_in_ms() - last_meal >= philos->data->time_to_die)
-	{
-		pthread_mutex_lock(&philos->data->write_lock);
-		printf("%ld %d died\n", get_time_in_ms() - philos->data->start_time,
-			philos->id);
-		pthread_mutex_lock(&philos->data->finish_lock);
-		philos->data->is_finish = 1;
-		pthread_mutex_unlock(&philos->data->finish_lock);
-		pthread_mutex_unlock(&philos->data->write_lock);
-		return (1);
-	}
-	return (0);
+	pthread_mutex_lock(&philos->data->write_lock);
+	printf("%ld %d died\n", get_time_in_ms() - philos->data->start_time,
+		philos->id);
+	pthread_mutex_lock(&philos->data->finish_lock);
+	philos->data->is_finish = 1;
+	pthread_mutex_unlock(&philos->data->finish_lock);
+	pthread_mutex_unlock(&philos->data->write_lock);
+	return (1);
 }
 
 void	monitor(t_philo *philos)
@@ -43,11 +34,16 @@ void	monitor(t_philo *philos)
 		philos->data->finish_eat = 0;
 		while (++i < philos->data->nb_philo)
 		{
-			if (check_death(&philos[i]))
-				return ;
 			pthread_mutex_lock(&philos[i].meal_lock);
+			if (get_time_in_ms()
+				- philos[i].last_meal_time >= philos->data->time_to_die)
+			{
+				pthread_mutex_unlock(&philos[i].meal_lock);
+				process_death(&philos[i]);
+				return ;
+			}
 			if (philos->data->goal_eat != -1
-				&& philos->data->goal_eat <= philos[i].nb_eat)
+				&& philos[i].nb_eat >= philos->data->goal_eat)
 				philos->data->finish_eat++;
 			pthread_mutex_unlock(&philos[i].meal_lock);
 		}
@@ -59,7 +55,7 @@ void	monitor(t_philo *philos)
 			pthread_mutex_unlock(&philos->data->finish_lock);
 			return ;
 		}
-		ft_usleep(1);
+		usleep(1000);
 	}
 }
 
